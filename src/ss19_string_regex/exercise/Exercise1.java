824569560e40b6da3487b3b0db84a7ac3c8b701d
Your Name <you@example.com>
2022-10-01 16:28:24 +0700

    nap bai tap ss19

--- src/ss19_string_regex/exercise/Exercise1.java
+++ src/ss19_string_regex/exercise/Exercise1.java
@@ -1,4 +1,16 @@
 package ss19_string_regex.exercise;
 
+import java.util.Scanner;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("nhập tên lớp :");
+        Scanner scanner = new Scanner(System.in);
+        String className = scanner.nextLine();
+        if (className.matches("^[ACP][0-9]{4}[G-M]$")){
+            System.out.println("tên lớp hợp lệ ");
+        }else {
+            System.out.println("tên lớp không hợp lệ !!!");
+        }
+    }
 }
--- src/ss19_string_regex/exercise/Exercise2.java
+++ src/ss19_string_regex/exercise/Exercise2.java
@@ -1,4 +1,17 @@
 package ss19_string_regex.exercise;
 
+import java.util.Scanner;
+
 public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println("hãy nhập số điện thoại :");
+        Scanner scanner = new Scanner(System.in);
+        String phoneNumber = String.valueOf(scanner.nextLine());
+
+        if (phoneNumber.matches("^[(][0-9]{2}[)][-][(][0][0-9]{9}[)]$")) {
+            System.out.println("số điện thoại chính xác ");
+        }else {
+            System.out.println("nhập sai định dạng số điện thoại!!!");
+        }
+    }
 }
